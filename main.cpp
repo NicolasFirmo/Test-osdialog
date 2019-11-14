03cd5cfa08120d2038fe5c6415b744b8f847ffb8
Nicolas <nicolasfofo@hotmail.com>
2019-11-14 21:19:49 -0200

    Modificações para windowns

--- main.cpp
+++ main.cpp
@@ -1,33 +1,10 @@
-#include <gtk/gtk.h>
 #include <osdialog.h>
 #include <iostream>
 
-static void
-activate(GtkApplication *app,
-         gpointer user_data)
-{
-  GtkWidget *window;
-
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Hello GNOME");
-  gtk_window_set_default_size(GTK_WINDOW(window), 500, 300);
-  gtk_widget_show_all(window);
-}
-
 int main(int argc, char *argv[])
 {
-  // GtkApplication *app;
-  int status;
 
-  // app = gtk_application_new("org.gtk.example",
-  //                           G_APPLICATION_FLAGS_NONE);
-  // g_signal_connect(app, "activate",
-  //                  G_CALLBACK(activate), NULL);
-  // status = g_application_run(G_APPLICATION(app), argc, argv);
-  // g_object_unref(app);
-
-  // auto file = osdialog_file(OSDIALOG_OPEN, NULL, NULL, NULL);
-  // std::cout << file << '\n';
+  std::cout << "test\n";
 
   osdialog_color color0 = {200, 200, 100};
   std::cout << "cor antes: "
@@ -44,5 +21,9 @@ int main(int argc, char *argv[])
             << (int)*((uint8_t *)(&color0) + sizeof(uint8_t) * 3)
             << '\n';
 
-  return (status);
+  char *path = osdialog_file(OSDIALOG_OPEN, NULL, NULL, NULL);
+
+  std::cout << "path: " << path << '\n';
+
+  return 0;
 }
