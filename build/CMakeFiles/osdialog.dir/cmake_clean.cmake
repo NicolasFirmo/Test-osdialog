f10399b676b23982b1f440d375e019b609c60963
NicolasFirmo <nicolasfofo@hotmail.com>
2019-11-14 19:33:18 -0300

    Commit inicial

---
+++ build/CMakeFiles/osdialog.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/osdialog.dir/osdialog/osdialog.c.o"
+  "CMakeFiles/osdialog.dir/osdialog/osdialog_gtk2.c.o"
+  "CMakeFiles/osdialog.dir/osdialog/osdialog_gtk3.c.o"
+  "libosdialog.a"
+  "libosdialog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/osdialog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
